b753408f1a997ec5d8bc01f2f988636f2d363ba1
MAIGA-web <lakarealassanekla@gmail.com>
2026-08-12 20:19:30 +0000

    Fix Laravel 12 trustProxies for Render HTTPSs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Faire confiance aux proxies Render pour récupérer les headers HTTPS
+        // Faire confiance aux proxies Render
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
