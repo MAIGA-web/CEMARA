92fc6537c43cd5f91e11147341a180546aa73404
MAIGA-web <lakarealassanekla@gmail.com>
2026-08-13 20:20:40 +0000

    Add defaults logo in public directory for Render

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -15,26 +15,31 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Token CSRF pour la sécurité Laravel -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>CƐMARA - {{ $fermeActive->fer_nom ?? 'Gestion Avicole' }}</title>
-    <meta name="description" content="Ela Admin - HTML5 Admin Template">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Méta-données de l'application -->
+    <meta name="description" content="CƐMARA - Plateforme de gestion avicole et de suivi de ferme.">
+    <meta name="author" content="CƐMARA">
+    <meta name="robots" content="noindex, nofollow">
 
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
-
+    <!-- Styles CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+    
     <link rel="stylesheet" href="{{ asset('assets/css/cs-skin-elastic.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
     <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
-
     <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
 
@@ -97,7 +102,7 @@
                 <div class="navbar-header">
 
                     <a class="navbar-brand" href="{{ route('dashboard') }}">
-                        <h2 class="logo-text font-20 text-primary font-weight-medium">CƐMARA</h2>
+                        <h2 class="logo-text font-10 text-primary font-weight-medium">CƐMARA</h2>
                     </a>
                     <a class="navbar-brand hidden" href="./"><img src="{{ asset('images/logo2.png') }}"
                             alt="Logo"></a>
@@ -107,9 +112,7 @@
             <div class="top-right">
                 <div class="header-menu">
                     <div class="header-left">
-                        <span class="font-20 badge badge-primary text-center font-weight-medium"> 
-                       FERME {{ session('fer_nom', auth()->user()->ferme->fer_nom ?? 'Aucune ferme sélectionnée') }}
-                        </span>
+                       
                         <button class="search-trigger"><i class="fa fa-search"></i></button>
                         <div class="form-inline">
                             <form class="search-form">
@@ -118,8 +121,8 @@
                                 <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
                             </form>
                         </div>
-                        <div class="user-area dropdown float-right">
-                            <span class="mr-2">
+                        <div class="user-area dropdown">
+                            <span>
                                 @if (auth()->user()->user_etat == 1)
                                     <span class="badge badge-danger">SUPER ADMIN</span>
                                 @else
@@ -144,7 +147,7 @@
                                     style="height: 35px; width: 35px; border-radius: 50%; object-fit: cover; border: 1px solid #18828a;">
                             @else
                                 <div class="mr-2 bg-primary text-white d-flex align-items-center justify-content-center font-weight-bold"
-                                    style="height: 35px; width: 35px; border-radius: 50%; font-size: 16px;">
+                                    style="height: 35px; width: 35px; border-radius: 50%; font-size: 10px;">
                                     {{ substr($fermeActive->fer_nom ?? 'C', 0, 1) }}
                                 </div>
                             @endif
